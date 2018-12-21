27bebfca32d5190ab7f480cb054f0904827e1f90
daysoul <daysoulzq@hotmail.com>
2018-12-21 12:37:51 +0800

    tony

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <?php if ($this->is('index')) : ?>
    <footer class="layoutSingleColumn layoutSingleColumn--wide footer" role="contentinfo"> 
     <div class="site-info"> 
-     <p>blog since 2018.&nbsp;&nbsp;&nbsp;&nbsp; I made this shit with ♥.<a href="<?php $this->options ->siteUrl(); ?>/log" class="cute" title="日志" target="_blank"> devlog</a>.
+     <p>blog since 2018.&nbsp;&nbsp;&nbsp;&nbsp; I made this shit with ♥.<a href=" https://daysoul.github.io/Theme-for-Typecho-PMPTD/" class="cute" title="日志" target="_blank"> devlog</a>.
 	 &nbsp;&nbsp;&nbsp;&nbsp;theme design by <a href="https://github.com/jozhn/Bigfa" class="cute" title="jozhn/Bigfa" target="_blank">jozhn/Bigfa</a>.</p>
     </div>
    </footer> 
